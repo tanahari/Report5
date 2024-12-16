fb1aa75d57da14c718d4348d1125785d25ff9ec8
Kamida SYOU <e245709@eve.u-ryukyu.ac.jp>
2024-12-16 15:31:18 +0900

    third commit

--- src/Report5.java
+++ src/Report5.java
@@ -1,11 +1,6 @@
 public class Report5 {
     public static void main(String[] args) {
-        try {
-            String str = null;
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("nuNullPointerExceptionllが発生しました。");
-            System.out.println(e.getMessage());
-        }
+        String str = "a";
+        int value = Integer.parseInt(str);
     }
 }
